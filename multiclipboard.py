c24ad4473abf249269e81cd36e26d6a3123aa40d
Michael Eskelin <michael.eskelin@gmail.com>
2022-04-13 17:30:29 -0400

    fix delete later

---
+++ multiclipboard.py
@@ -0,0 +1,45 @@
+import sys, clipboard, json
+
+cPath = "cont.json"
+
+def save(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f)
+
+def load(path):
+    try:
+        with open(path, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+def delete(path, key):
+    with open(path, "r") as f:
+        data = json.load(f)
+    for element in data:
+        element.pop(key, None)
+
+if len(sys.argv) == 2:
+    data = load(cPath)
+    
+    if sys.argv[1] == "lo":
+        k = input("Key: ")
+        if k in data:
+            clipboard.copy(data[k])
+        else:
+            print("Key not found")
+    elif sys.argv[1] == "s":
+        k = input("Key: ")
+        data[k] = clipboard.paste()
+        save(cPath, data)
+        print(f"Saved under key: {k}")
+    elif sys.argv[1] == "li":
+        print(data)
+    elif sys.argv[1] == "d":
+        k = input("Key: ")
+        delete(cPath, k)
+    else:
+        print("Unknown command.")
+else:
+    print("Too many/few arguments")
